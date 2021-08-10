aaeb5845e0a16136f16cca5d84ad27d9f7c251bf
Alliot404 <abhilashramu1999@gmail.com>
2021-08-10 09:22:11 +0530

    incorporate-logging

--- spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -4,34 +4,33 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringLearnApplication.class, args);
 		displayDate();
 	}
-	
-	static void displayDate()  {
-		 ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
-		 SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
-		 try {
+
+	static void displayDate() {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		try {
 			Date parseDate = format.parse("31/12/2018");
-			System.out.println(parseDate);
+			// System.out.println(parseDate);
+			LOGGER.debug(parseDate.toString());
 		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		 
-		 SimpleDateFormat format1 = context.getBean("dateFormat1", SimpleDateFormat.class);
-		 try {
-			Date parseDate1 = format1.parse("08/09/2021");
-			System.out.println(parseDate1);
-		} catch (ParseException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
