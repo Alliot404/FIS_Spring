6932c166747eb90712d835f678cc0d405314c966
Alliot404 <abhilashramu1999@gmail.com>
2021-08-10 15:55:46 +0530

    pkgs added/modified in layers

--- spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -7,25 +7,47 @@ import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.fis.springlearn.bean.Employee;
+import com.fis.springlearn.controller.EmployeeController;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
-		displayDate();
-		displayCountry();
-		displayCountries();
-		displayEmployee();
+//		SpringApplication.run(SpringLearnApplication.class, args);
+//		displayDate();
+//		displayCountry();
+//		displayCountries();
+//		displayEmployee();
+		getEmployeeController();
+
+//		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(EmployeeController.class);
+//		displayEmployeeControllerAnnotation(applicationContext);
 	}
-	
+
+	public static void displayEmployeeControllerAnnotation(ApplicationContext applicationContext) {
+		LOGGER.info("START");
+		EmployeeController employeeController = applicationContext.getBean("employeeController",
+				EmployeeController.class);
+		LOGGER.debug("EmployeeController : {}", employeeController);
+		LOGGER.info("END");
+
+	}
+
+	static void getEmployeeController() {
+		LOGGER.info("START");
+		ApplicationContext context = new ClassPathXmlApplicationContext("employee.xml");
+		EmployeeController employeeController = context.getBean("controller", EmployeeController.class);
+		LOGGER.debug("EmployeeController : {}", employeeController);
+		LOGGER.info("END");
+	}
+
 	static void displayEmployee() {
 		LOGGER.info("START");
 		ApplicationContext context = new ClassPathXmlApplicationContext("employee.xml");
@@ -56,7 +78,7 @@ public class SpringLearnApplication {
 		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
 		try {
 			Date parseDate = format.parse("31/12/2018");
-			System.out.println(parseDate);
+//			System.out.println(parseDate);
 			LOGGER.debug(parseDate.toString());
 		} catch (ParseException e) {
 			e.printStackTrace();
@@ -65,7 +87,7 @@ public class SpringLearnApplication {
 		SimpleDateFormat format1 = context.getBean("dateFormat", SimpleDateFormat.class);
 		try {
 			Date parseDate1 = format1.parse("08/09/2021");
-			System.out.println(parseDate1);
+//			System.out.println(parseDate1);
 			LOGGER.debug(parseDate1.toString());
 		} catch (ParseException e) {
 			e.printStackTrace();
