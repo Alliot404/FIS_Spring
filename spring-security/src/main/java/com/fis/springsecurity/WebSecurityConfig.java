23b01452c738b5a599cee781c849d277b90902fd
Alliot404 <abhilashramu1999@gmail.com>
2021-08-13 10:59:24 +0530

    testing

--- spring-security/src/main/java/com/fis/springsecurity/WebSecurityConfig.java
+++ spring-security/src/main/java/com/fis/springsecurity/WebSecurityConfig.java
@@ -1,4 +1,39 @@
+/*
+ * package com.fis.springsecurity;
+ * 
+ * import org.springframework.context.annotation.Bean; import
+ * org.springframework.context.annotation.Configuration; import
+ * org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ * import org.springframework.security.config.annotation.web.configuration.
+ * EnableWebSecurity; import
+ * org.springframework.security.config.annotation.web.configuration.
+ * WebSecurityConfigurerAdapter; import
+ * org.springframework.security.core.userdetails.User; import
+ * org.springframework.security.core.userdetails.UserDetails; import
+ * org.springframework.security.core.userdetails.UserDetailsService; import
+ * org.springframework.security.provisioning.InMemoryUserDetailsManager;
+ * 
+ * @Configuration
+ * 
+ * @EnableWebSecurity public class WebSecurityConfig extends
+ * WebSecurityConfigurerAdapter {
+ * 
+ * @Override protected void configure(HttpSecurity http) throws Exception { http
+ * .authorizeRequests() .antMatchers("/", "/home","/emp/all").permitAll()
+ * .anyRequest().authenticated() .and() .formLogin() .loginPage("/login")
+ * .permitAll() .and() .logout() .permitAll(); }
+ * 
+ * @Bean
+ * 
+ * @Override public UserDetailsService userDetailsService() { UserDetails user =
+ * User.withDefaultPasswordEncoder() .username("user") .password("password")
+ * .roles("USER") .build();
+ * 
+ * return new InMemoryUserDetailsManager(user); } }
+ */
+
 package com.fis.springsecurity;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,25 +52,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/home","/emp/all").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
+				.antMatchers("/", "/home","/products","/welcome").permitAll()
+				.anyRequest().authenticated().and()
+				.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
 	@Bean
 	@Override
 	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
+		UserDetails user = User.withDefaultPasswordEncoder().username("abhi").password("abhi").roles("USER")
 				.build();
 
 		return new InMemoryUserDetailsManager(user);
